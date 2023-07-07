81695960b6945af86adfeb7703475aea79ab3e01
Edmund Walsh <ewalsh@economicdatasciences.com>
2023-07-07 15:45:58 +0100

    starting ch2

--- app/src/main/java/ai/economicdatasciences/howtoandroidch1/MainActivity.kt
+++ app/src/main/java/ai/economicdatasciences/howtoandroidch1/MainActivity.kt
@@ -14,34 +14,69 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.enter_button)?.setOnClickListener {
-            // get the greeting display text
-            val greetingDisplay = findViewById<TextView>(R.id.greeting_display)
-            // get the first name TextInputEditText value
-            val firstName = findViewById<TextInputEditText>(R.id.first_name)?.text.toString().trim()
-            // get the last name
-            val lastName = findViewById<TextInputEditText>(R.id.last_name)?.text.toString().trim()
-            // checking name is not empty
-            if(firstName.isNotEmpty() && lastName.isNotEmpty()){
-                val nameToDisplay = firstName.plus(" ").plus(lastName)
-                // string template to display
-                greetingDisplay?.text = " ${getString(R.string.welcome_to_the_app)} ${nameToDisplay}!"
-            } else {
-                Toast.makeText(this, getString(R.string.please_enter_a_name), Toast.LENGTH_LONG).apply {
-                    setGravity(Gravity.CENTER, 0, 0)
-                    show()
-                }
-            }
-        }
+        // findViewById<Button>(R.id.enter_button)?.setOnClickListener {
+        //     // get the greeting display text
+        //     val greetingDisplay = findViewById<TextView>(R.id.greeting_display)
+        //     // get the first name TextInputEditText value
+        //     val firstName = findViewById<TextInputEditText>(R.id.first_name)?.text.toString().trim()
+        //     // get the last name
+        //     val lastName = findViewById<TextInputEditText>(R.id.last_name)?.text.toString().trim()
+        //     // checking name is not empty
+        //     if(firstName.isNotEmpty() && lastName.isNotEmpty()){
+        //         val nameToDisplay = firstName.plus(" ").plus(lastName)
+        //         // string template to display
+        //         greetingDisplay?.text = " ${getString(R.string.welcome_to_the_app)} ${nameToDisplay}!"
+        //     } else {
+        //         Toast.makeText(this, getString(R.string.please_enter_a_name), Toast.LENGTH_LONG).apply {
+        //             setGravity(Gravity.CENTER, 0, 0)
+        //             show()
+        //         }
+        //     }
+        // }
         Log.d(TAG, "onCreate")
     }
 
     override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart")
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreIntanceState")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreIntanceState")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 
     companion object {
-        private const val TAG = "MainActivity"
+        private const val TAG = "MainActivity"
     }
 }
